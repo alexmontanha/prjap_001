d1a6978944e449ce3d6ce64224462f43545fda31
Alexandre de Oliveira <alexmontanha@hotmail.com>
2023-09-16 11:36:01 -0300

    Grava os dados da peça em arquivo

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-
         ArrayList<Peca> pecas = new ArrayList<Peca>();
 
 
@@ -13,40 +12,32 @@ public class App {
         Scanner teclado = new Scanner(System.in);
 
 
-        Peca peca1 = new Peca();
-        peca1.quantidadeEstoque = 0;
+        //Cria um loop para entrar com várias peças
+        //Se o código for 0, sai do loop
+        while (true) {
+            System.out.println("Digite o código da peça: ");
+            int codigo = teclado.nextInt();
+            if (codigo == 0) {
+                break;
+            }
+            System.out.println("Digite o nome da peça: ");
+            String nome = teclado.next();
+            System.out.println("Digite a descrição da peça: ");
+            String descricao = teclado.next();
+            System.out.println("Digite o preço unitário da peça: ");
+            double precoUnitario = teclado.nextDouble();
+            System.out.println("Digite a quantidade em estoque da peça: ");
+            int quantidadeEstoque = teclado.nextInt();
 
-        System.out.println("Digite o código da peça: ");
-        peca1.codigo = teclado.nextInt();
-        System.out.println("Digite o nome da peça: ");
-        peca1.nome = teclado.next();
-        System.out.println("Digite a descrição da peça: ");
-        peca1.descricao = teclado.next();
-        System.out.println("Digite o preço unitário da peça: ");
-        peca1.precoUnitario = teclado.nextDouble();
+            Peca peca = new Peca();
+            peca.codigo = codigo;
+            peca.nome = nome;
+            peca.descricao = descricao;
+            peca.precoUnitario = precoUnitario;
+            peca.quantidadeEstoque = quantidadeEstoque;
 
-
-        Peca peca2 = new Peca();
-        peca2.quantidadeEstoque = 0;
-
-        // Aceitar via teclado as informações para cadastrar uma peça
-        // Informações: código, nome, descrição, preço unitário e quantidade em estoque
-
-        System.out.println("Digite o código da peça: ");
-        peca2.codigo = teclado.nextInt();
-        System.out.println("Digite o nome da peça: ");
-        peca2.nome = teclado.next();
-        System.out.println("Digite a descrição da peça: ");
-        peca2.descricao = teclado.next();
-        System.out.println("Digite o preço unitário da peça: ");
-        peca2.precoUnitario = teclado.nextDouble();
-
-
-        // Coloca a peça em um Objeto
-        // Criar um ArrayList para armazenar as peças
-        // Adicionar a peça no ArrayList
-        pecas.add(peca1);
-        pecas.add(peca2);
+            pecas.add(peca);
+        }
 
         //Iterar no ArrayList e imprimir as informações de cada peça
         for (Peca peca : pecas) {
@@ -73,10 +64,27 @@ public class App {
             // Calcular o valor do estoque da peça
             System.out.println("Valor do estoque: " + peca.calcularValorEstoque());
         }
-
-
         teclado.close();
 
+        // Verificar se já existe um arquivo chamado pecas.txt
+        // Se não existir, criar o arquivo
+        // Se existir, abrir o arquivo para escrita
+        // Escrever no arquivo as informações de cada peça
+        // Fechar o arquivo
+        File arquivo = new File("pecas.txt");
+        if (!arquivo.exists()) {
+            arquivo.createNewFile();
+        }
+        java.io.FileWriter escritor = new java.io.FileWriter(arquivo);
+        for (Peca peca : pecas) {
+            escritor.write("Código: " + peca.codigo + "\n");
+            escritor.write("Nome: " + peca.nome + "\n");
+            escritor.write("Descrição: " + peca.descricao + "\n");
+            escritor.write("Preço Unitário: " + peca.precoUnitario + "\n");
+            escritor.write("Quantidade em estoque: " + peca.quantidadeEstoque + "\n");
+            escritor.write("Valor do estoque: " + peca.calcularValorEstoque() + "\n");
+        }
+        escritor.close();
 
     }
 }
